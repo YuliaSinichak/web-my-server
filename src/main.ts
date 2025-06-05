1c34ea5cad9ff2732f7268ea1f31fa489d0e6c0d
YuliaSinichak <yuliasinichak@gmail.com>
2025-06-06 00:01:21 +0300

    qwfewrtn

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+ app.enableCors({
+  origin: 'https://web-client-ruby.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  credentials: true,
+});
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
