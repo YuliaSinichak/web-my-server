922669993587bda405539bf48f0e4c8bf88c558c
YuliaSinichak <yuliasinichak@gmail.com>
2025-06-05 23:37:27 +0300

    sdfedsvfcsdvsd

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FirebaseModule } from './firebase/firebase.module';
 import { BuildingsModule } from './buildings/buildings.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [FirebaseModule, BuildingsModule],
+  imports: [FirebaseModule, BuildingsModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/firebase/firebase.module.ts
+++ src/firebase/firebase.module.ts
@@ -1,20 +1,23 @@
 import { Module, Global } from '@nestjs/common';
 import * as admin from 'firebase-admin';
 
-
-
 @Global()
 @Module({
   providers: [
     {
       provide: 'FIRESTORE',
       useFactory: () => {
-        // Initialize app only once
+        console.log('FIREBASE_CONFIG env var:', process.env.FIREBASE_CONFIG); // debug
+
+        if (!process.env.FIREBASE_CONFIG) {
+          throw new Error('FIREBASE_CONFIG env var is missing!');
+        }
+
         if (admin.apps.length === 0) {
+         const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG!);
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
           admin.initializeApp({
-            credential: admin.credential.cert(
-              require('../../newwebtech-1d5e4-firebase-adminsdk-fbsvc-d30e19118f.json'),
-            ),
+            credential: admin.credential.cert(serviceAccount),
           });
         }
         return admin.firestore();
